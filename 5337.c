0051f1d7e75fe2dbda7e3177f351da14e830ad3a
Gasiyori <75403856+Gasiyori@users.noreply.github.com>
2021-03-12 19:29:41 +0900

    Add files via upload

---
+++ 5337.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#pragma warning (disable : 4996)
+
+int main() {
+	printf(".  .   .\n");
+	printf("|  | _ | _. _ ._ _  _\n");
+	printf("|/\\|(/.|(_.(_)[ | )(/.");
+}
+
+/*
+.  .   .
+|  | _ | _. _ ._ _  _
+|/\|(/.|(_.(_)[ | )(/.
+*/
