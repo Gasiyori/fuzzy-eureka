38847c4b77cbf97b4613afe19c5f96e99459f23d
Gasiyori <75403856+Gasiyori@users.noreply.github.com>
2021-03-12 19:14:19 +0900

    Add files via upload

---
+++ 3003.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#pragma warning(disable:4996)
+
+int main() {
+	int king, queen, rook, bishop, knight, pawn;
+
+	scanf("%d %d %d %d %d %d", &king, &queen, &rook, &bishop, &knight, &pawn);
+
+	printf("%d %d %d %d %d %d", 1-king, 1-queen, 2-rook, 2-bishop, 2-knight, 8-pawn);
+}
